7e53067202ce61fb62b6f2900407559eebf98241
bibek1414 <bbhattarai770@gmail.com>
2025-04-28 15:18:02 +0545

    insalled freamer motion

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion"
+function App() {
+  
+  return (
+   <>
+    <motion.div>
+      I'will move 100px to right
+      </motion.div>   
+   </>
+  )
+}
+
+export default App
